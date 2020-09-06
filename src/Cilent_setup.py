5475de26304a906879621e215af78f08c3bf6c4c
jagdishwar <jagdishwarbiradar@gmail.com>
2020-09-06 19:27:57 +0530

    adding chatbot and texttospeech converter

--- src/Cilent_setup.py
+++ src/Cilent_setup.py
@@ -1,12 +1,19 @@
 import socket
+import os
+
+
 
 MAX_SIZE_BYTES = 65535 # Mazimum size of a UDP datagram
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = input('Input lowercase sentence:' )
-data = message.encode('ascii')
-s.sendto(data, ('127.0.0.1', 3000))
-print('The OS assigned the address {} to me'.format(s.getsockname()))
-data, address = s.recvfrom(MAX_SIZE_BYTES)
-text = data.decode('ascii')
-print('The server {} replied with {!r}'.format(address, text))
+count=4
+while count:
+    message = input('Input lowercase sentence:' )
+    data = message.encode('ascii')
+
+    s.sendto(data, ('127.0.0.1', 3000))
+    print('The OS assigned the address {} to me'.format(s.getsockname()))
+    data, address = s.recvfrom(MAX_SIZE_BYTES)
+    text = data.decode('ascii')
+    print('The server {} replied with {!r}'.format(address, text))
+    count-=1
